8e934005cd2b5b7b7dced0575c54c7a72da9b782
Fez04 <david_avendano@hotmail.com>
2017-12-05 18:12:38 -0800

    background
    deleted background

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  // $("#services").fadeout(2000);
+ $(".container").ready(function(){
+      $("#services").fadeOut(2000);
+});
+});
