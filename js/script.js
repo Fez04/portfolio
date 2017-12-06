6fd790a7d3dacc4950ce4ec5320f5028560b48d3
david <david_avendano@hotmail.com>
2017-12-05 23:00:45 -0800

    icons
    fadein icons looks dope!!

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  $(".icon").hide();
+    $("h4").hide();
+  $(".icon").show(2000);
+  $("h4").show(2000);
+
+
+});
